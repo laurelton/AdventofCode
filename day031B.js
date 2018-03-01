de10acd6fc6c763fad3d5801a4aa612347300ebc
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-01 17:06:31 -0500

    store locations

--- day031B.js
+++ day031B.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let numbers = [1, 2, 3, 4, 12, 23, 1024, 312051];
+let numbers = [5, 6, 7, 8, 9, 10, 12, 23, 1024, 312051];
 
 class Vector {
     constructor(x, y) {
@@ -12,42 +12,100 @@ class Vector {
         return new Vector(this.x + vector.x, this.y + vector.y);
     }
 
-    // subtract(vector) {
-    //     this.x -= vector.x;
-    //     this.y -= vector.y;
-    // }
+    subtract(vector) {
+        return new Vector(this.x - vector.x, this.y - vector.y);
+    }
 
     taxiPath(vector) {
         return (this.x - vector.x) + (this.y - vector.y);
     }
+
+    equals(vector) {
+        return this.x == vector.x && this.y == vector.y;
+    }
 }
 
-let right = new Vector( 1,  0);
-let up    = new Vector( 0,  1);
-let left  = new Vector(-1,  0);
-let down  = new Vector( 0, -1);
+let right = new Vector(1, 0);
+let up    = new Vector(0, 1);
+let left  = new Vector(-1, 0);
+let down  = new Vector(0, -1);
+
+// let DIRECTIONS = {
+//     CURRENT: right,
+//     0: right,
+//     1: up,
+//     2: left,
+//     3: down
+// }
 
 let directions = [right, up, left, down];
 let currentDirection = 0;
-let origin = new Vector(0,0);
+let origin = new Vector(0, 0);
+let filledCoordinates = [];
+filledCoordinates.push(origin);
 
 let spiral = {
     1: origin
 };
 
+function changeDirection(currentValue) {
+    // let location = spiral[currentValue];
+    currentDirection = (currentDirection + 1) % 4;
+}
+
+function turnLeft() {    
+    currentDirection = (currentDirection + 1) % 4;
+}
+
 numbers.forEach(number => {
-    for(let i = 1; i <= number; i++) {
+    let upNeighbor, leftNeighbor, downNeighbor, rightNeighbor, neighborExists;
+    for (let i = 1; i <= number; i++) {
         if (i in spiral) {
-            // console.log(`Number = ${i} Steps = ${spiral[i].taxiPath(origin)}`);
             continue;
         } else {
-            spiral[i] = spiral[i-1].add(directions[currentDirection]);
-            let next = i + 1;
-            if (!(next in spiral)) {
-                currentDirection = (currentDirection + 1) % 4;
+            spiral[i] = spiral[i - 1].add(directions[currentDirection]);
+            filledCoordinates.push(spiral[i]);
+                        
+            switch (directions[currentDirection]) {
+                case right:
+                    upNeighbor = spiral[i].add(up);
+                    neighborExists = filledCoordinates.find(elem => {
+                        return elem.equals(upNeighbor);
+                    });
+                    if (neighborExists == null) {
+                        changeDirection(i);
+                    }
+                    break;
+                case up:
+                    leftNeighbor = spiral[i].add(left);
+                    neighborExists = filledCoordinates.find(elem => {
+                        return elem.equals(upNeighbor);
+                    });
+                    if (neighborExists == null) {
+                        changeDirection(i);
+                    }
+                    break;
+                case left:
+                    downNeighbor = spiral[i].add(down);
+                    neighborExists = filledCoordinates.find(elem => {
+                        return elem.equals(upNeighbor);
+                    });
+                    if (neighborExists == null) {
+                        changeDirection(i);
+                    }
+                    break;
+                case down:
+                    rightNeighbor = spiral[i].add(right);
+                    neighborExists = filledCoordinates.find(elem => {
+                        return elem.equals(upNeighbor);
+                    });
+                    if (neighborExists == null) {
+                        changeDirection(i);
+                    }
+                    break;
             }
         }
     }
-    
+
     console.log(`Number = ${number}      Steps = ${spiral[number].taxiPath(origin)}`);
 });
