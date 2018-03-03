e245204e6cd3b1951e953bc653e3e98887edc921
laurelton <777443+laurelton@users.noreply.github.com>
2018-03-03 17:16:36 -0500

    Make directories

---
+++ directories.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+for year in 2015 2016 2017; do mkdir $year; for day in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25; do mkdir ${year}/${day} ; done; done
